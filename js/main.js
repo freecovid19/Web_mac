11bfe0cea4dc1cb7576afe8200714937e9acb38b
freecovid19 <77673431+freecovid19@users.noreply.github.com>
2021-09-17 09:22:17 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,1 @@
-// booting
-var target = document.getElementsByClassName("booting");
-
-setTimeout(function() {bootFin()}, 1500);
-
-function bootFin(){
-    target.style.display = 'none';
-}
+// This file will be coded later.
